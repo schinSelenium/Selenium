c57eccee15be673a51ad22524c68c81a8d0e5eca
schinSelenium <sachin.nagtilak03@gmail.com>
2020-12-09 17:38:53 +0530

    new commit

---
+++ src/javaprograms/GitDemo.java
@@ -0,0 +1,9 @@
+package javaprograms;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+//This is to take the GIt update added new class and comminting the code, previoussly commited all code 
+	}
+
+}
